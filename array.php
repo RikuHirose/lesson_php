c844a1bec359aea3d76fb8689120dbd52bce65e1
RikuHirose <rikuparkour9@gmail.com>
2018-10-10 01:31:16 +0700

    first commit

---
+++ array.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$data = [
+  [
+    'id'    => 1,
+    'item'  => 'apple',
+    'price' => 210
+  ],
+  [
+    'id'    => 2,
+    'item'  => 'water',
+    'price' => 130
+  ],
+
+  [
+    'id'    => 3,
+    'item'  => 'mochi',
+    'price' => 204
+  ],
+
+  [
+    'id'    => 4,
+    'item'  => 'coke',
+    'price' => 240
+  ],
+
+  [
+    'id'    => 5,
+    'item'  => 'milk',
+    'price' => 520
+  ],
+
+];
+
+// Question 上記の配列からpriceの合計をforeach文を使って算出せよ
---
+++ index.php
@@ -0,0 +1,113 @@
+<?php
+// echo 'hello';
+// echo "hello";
+// echo '1';
+// echo 1;
+
+
+
+// 変数
+// $num = 1;
+// $name = 'taro';
+
+// echo $num;
+// echo $greet;
+
+// echo $greet.$num;
+
+
+
+// 文字列を連結
+// echo 'hello'.'world';
+// echo 'hello'.$name;
+
+
+// 演算子
+
+  // echo 1 + 1; // 加算
+  // echo 6 - 3; // 減算
+  // echo 3 * 4; // 積
+  // echo 12 / 2; // 商
+  // echo 13 % 2; // 剰余
+
+
+// 関数
+
+  // function calc($a, $b) {
+  //   $total = $a + $b;
+  //   echo $total;
+  // }
+
+  // calc(1, 3);
+
+// if
+
+  // $v = 'programing';
+  // if($v == 'programing') {
+  //   echo 1;
+  // } else if($v == 'PHP') {
+  //   echo 2;
+  // } else {
+  //   echo 3;
+  // }
+
+
+// for
+  for ($i=0; $i < 100; $i++) {
+    echo $i;
+  }
+
+
+
+
+
+
+
+// 配列
+  $array = ['black', 'red', 'blue'];
+  // echo $array[0];
+  // echo $array[1];
+  // echo $array[2];
+
+
+// question
+  $words = ['orange', 'apple', 'water', 'test', 'aaaa'];
+
+  // waterを出力せよ
+
+// 連想配列
+$data = [
+  [
+    'id' => 1,
+    'name' => 'taro',
+  ],
+  [
+    'id' => 2,
+    'name' => 'jiro',
+  ],
+
+  [
+    'id' => 3,
+    'name' => 'sabu',
+  ],
+
+  [
+    'id' => 4,
+    'name' => 'tom',
+  ],
+
+  [
+    'id' => 5,
+    'name' => 'jena',
+  ],
+
+];
+// foreach
+// Question 上記の配列からnameのみを出力せよ
+
+
+
+
+
+// array.php
+// fizzbuzz.php
